5c55e8ac8374b7279821015ac56400351c5626b6
jseawall <jseawall@numurus.com>
2023-10-18 12:02:22 -0700

    Update to Latest
    Update to Latest

--- nepi_file_system_config_device_info.sh
+++ nepi_file_system_config_device_info.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-# This bash scrict updates a NEPI device's Serial Number
+# This bash scrict updates a NEPI device's information
 # You must reboot your device after updating for changes to take effect
 
+rm /opt/nepi/.sys_env.bash.swp
 #####################################################
-### Edit Serial Number Below
+### Edit Below, Or Comment Out Lines to Skip
 #####################################################
 ###--Serial Number Updating ------------------------------------------------------
 Serial_Number=100100
+sed -i "s/DEVICE_SN=.*/DEVICE_SN=$Serial_Number/" /opt/nepi/sys_env.bash
+###--Device ID Updating ------------------------------------------------------
+ID=S2X
+sed -i "s/DEVICE_ID=.*/DEVICE_ID=$ID/" /opt/nepi/sys_env.bash
 #####################################################
 
-rm /opt/nepi/.sys_env.bash.swp
-sed -i "s/DEVICE_SN=.*/DEVICE_SN=$Serial_Number/" /opt/nepi/sys_env.bash
+
+
 
