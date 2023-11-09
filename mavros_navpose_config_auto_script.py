e8da2d735c8068a19f2833abf5035685e1f43946
jseawall <jseawall@numurus.com>
2023-11-09 10:26:25 -0800

    Name Update

--- mavros_navpose_config_auto_script.py
+++ mavros_navpose_config_auto_script.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
 
 # Sample NEPI Automation Script.
-# Uses onboard ROS python library to
-# 1. Connect NEPI NavPose topics to appropriate MAVLink topics
+# Uses onboard ROS python and mavros libraries to
+# 1. Connect NEPI NavPose topics to appropriate mavros topics
 
 import rospy
 from std_msgs.msg import String, Empty
@@ -14,16 +14,17 @@ from sensor_msgs.msg import NavSatFix
 
 # ROS namespace setup
 BASE_NAMESPACE = "/nepi/s2x/"
+MAVROS_NAMESPACE = BASE_NAMESPACE + "pixhawk_mavlink/"
 
 ### Uncomment This Section for Ardupilot Firmware
-MAVLINK_GPS_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/global_position/global"
-MAVLINK_HEADING_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/global_position/compass_hdg"
-MAVLINK_ORIENTATION_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/global_position/local"
+MAVROS_GPS_TOPIC = MAVROS_NAMESPACE + "global_position/global"
+MAVROS_HEADING_TOPIC = MAVROS_NAMESPACE + "global_position/compass_hdg"
+MAVROS_ORIENTATION_TOPIC = MAVROS_NAMESPACE + "global_position/local"
 
 ### Uncomment This Section for Pixhawk Firmware
-##MAVLINK_GPS_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/global_position/global"
-##MAVLINK_HEADING_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/global_position/compass_hdg"
-##MAVLINK_ORIENTATION_TOPIC = BASE_NAMESPACE + "pixhawk_mavlink/local_position/odom"
+##MAVROS_GPS_TOPIC = MAVROS_NAMESPACE + "global_position/global"
+##MAVROS_HEADING_TOPIC = MAVROS_NAMESPACE + "global_position/compass_hdg"
+##MAVROS_ORIENTATION_TOPIC = MAVROS_NAMESPACE + "local_position/odom"
 
 ### Setup NEPI NavPose Topic Namespaces
 SET_NAVPOSE_GPS_TOPIC = BASE_NAMESPACE + "nav_pose_mgr/set_gps_fix_topic"
@@ -42,15 +43,15 @@ def startNode():
   # Update GPS source
   gps_pub = rospy.Publisher(SET_NAVPOSE_GPS_TOPIC, String, queue_size=10)
   rospy.sleep(1) # VERY IMPORTANT - Sleep a bit between declaring a publisher and using it subscribers have time to subscribe
-  gps_pub.publish(MAVLINK_GPS_TOPIC)
+  gps_pub.publish(MAVROS_GPS_TOPIC)
   # Update Heading source
   heading_pub = rospy.Publisher(SET_NAVPOSE_HEADING_TOPIC, String, queue_size=10)
   rospy.sleep(1) # VERY IMPORTANT - Sleep a bit between declaring a publisher and using it subscribers have time to subscribe
-  heading_pub.publish(MAVLINK_HEADING_TOPIC)
-  # Update Oreantation source
+  heading_pub.publish(MAVROS_HEADING_TOPIC)
+  # Update Orientation source
   orientation_pub = rospy.Publisher(SET_NAVPOSE_ORIENTATION_TOPIC, String, queue_size=10)
   rospy.sleep(1) # VERY IMPORTANT - Sleep a bit between declaring a publisher and usin
-  orientation_pub.publish(MAVLINK_ORIENTATION_TOPIC)
+  orientation_pub.publish(MAVROS_ORIENTATION_TOPIC)
 
   # Sleep a bit so that the publisher threads above have time to do their work before this script exits
   rospy.loginfo("Sleeping to let publishers finish")
